05c01b7955693153cd6031499ffa6dac3c214812
alexandersafstrom <71256691+alexandersafstrom@users.noreply.github.com>
2020-09-14 12:30:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ var password="5f4dcc3b5aa765d61d8327deb882cf99";
         userInput= $.MD5($('#input').val());
         
         if (userInput==password){
-                  $("main").css("display:all; z-index:20;");
+                  $(".main").css("display:all; z-index:20;");
 
         } else {
-         $("main").css("display:none; z-index:-20;");
+         $(".main").css("display:none; z-index:-20;");
         }
       });
       
