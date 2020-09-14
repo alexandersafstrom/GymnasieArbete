f8c1462efbc46d27cca68394f96f10cbb5f27b8d
alexandersafstrom <71256691+alexandersafstrom@users.noreply.github.com>
2020-09-14 12:28:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var password="5f4dcc3b5aa765d61d8327deb882cf99";
         userInput= $.MD5($('#input').val());
         
         if (userInput==password){
-                  $("main").css({"display:all; z-index:20;"});
+                  $("main").css("display:all; z-index:20;");
 
         } else {
          $("main").css({"display:none; z-index:-20;"});
