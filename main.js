31e52d06ac2d071e1e231c430cf8dd0bf11720d7
alexandersafstrom <71256691+alexandersafstrom@users.noreply.github.com>
2020-09-14 12:42:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ var password="5f4dcc3b5aa765d61d8327deb882cf99";
     jQuery(document).ready(function($) {
             console.log("Document loaded!");
             if (userInput==password){
-                  $(".main").css("display:all; z-index:20;");
+                  $("#main").css("display:all; z-index:20;");
                         console.log("userInput is equal to password");
         } else {
-         $(".main").css("display:none; z-index:-20;");
+         $("#main").css("display:none; z-index:-20;");
                                         console.log("userInput is not equal to password");
 
         }
@@ -16,10 +16,10 @@ var password="5f4dcc3b5aa765d61d8327deb882cf99";
         userInput= $.MD5($('#input').val());
         
         if (userInput==password){
-                  $(".main").css("display:all; z-index:20;");
+                  $("#main").css("display:all; z-index:20;");
 
         } else {
-         $(".main").css("display:none; z-index:-20;");
+         $("#main").css("display:none; z-index:-20;");
         }
       });
       
